49c5e7847f73baebe0452cc6c1ee75bb14e374b7
Lucas <szanonn@gmail.com>
2023-09-27 12:23:50 -0300

    feat: added about section

---
+++ components/AboutSection.vue
@@ -0,0 +1,91 @@
+<template>
+  <section>
+    <v-container fluid>
+      <v-row align="center" justify="center">
+        <v-col cols="10">
+          <v-row align="center" justify="center">
+            <v-col cols="12" md="7">
+              <h1 class="font-weight-thin display-2">Sobre</h1>
+              <h1 class="font-weight-thin display-1 mb-3">
+                Lorem ipsum dolor!
+              </h1>
+              <v-row>
+                <v-col cols="12" class="d-flex align-center">
+                  <v-img
+                    :src="img_rocket"
+                    max-width="60px"
+                    class="mr-4"
+                  />
+                  <p class="text-justify about-text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  </p>
+                </v-col>
+                <v-col cols="12" class="d-flex align-center">
+                  <v-img
+                    :src="img_webchat"
+                    max-width="60px"
+                    class="mr-4"
+                  />
+                  <p class="text-justify about-text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  </p>
+                </v-col>
+                <v-col cols="12" class="d-flex align-center">
+                  <v-img
+                    :src="img_handshake"
+                    max-width="60px"
+                    class="mr-4"
+                  />
+                  <p class="text-justify about-text">
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                    Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  </p>
+                </v-col>
+              </v-row>
+            </v-col>
+            <v-col cols="12" md="5" class="d-none d-md-flex">
+              <v-img
+                :src="img_team"
+                class="d-block ml-auto mr-auto"
+                max-width="400px"
+              />
+            </v-col>
+          </v-row>
+        </v-col>
+      </v-row>
+    </v-container>
+  </section>
+</template>
+
+<script>
+import handshake_svg from "~~/assets/img/handshake.svg";
+import rocket_svg from "~~/assets/img/rocket-launch.svg";
+import webchat_svg from "~~/assets/img/wechat.svg";
+import team_png from "~~/assets/img/team.png";
+
+export default {
+  data() {
+    return {
+      img_team: team_png,
+      img_rocket: rocket_svg,
+      img_handshake: handshake_svg,
+      img_webchat: webchat_svg
+    };
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: xx-large;
+}
+
+.about-text {
+  max-width: 30em;
+}
+</style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,31 @@
 <template>
   <navigation />
-      <home />
+  <div class="pt-0 index-main-container">
+    <home />
+    <about />
+  </div>
 </template>
+
+<style scoped>
+.index-main-container {
+  background-image: url("~~/assets/img/bgMain.png");
+  background-attachment: fixed;
+  background-position: center;
+  background-size: cover;
+}
+</style>
 
 <script>
 import navigation from "~~/components/Navigation.vue";
 import home from "~~/components/HomeSection.vue";
+import about from "~~/components/AboutSection.vue";
 
 export default {
   name: "index",
   components: {
     navigation,
-    home
+    home,
+    about,
   },
 };
 </script>
