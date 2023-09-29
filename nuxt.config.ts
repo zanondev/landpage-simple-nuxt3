b198422edfa32088e02eeed27a118a88b139c2c9
Lucas <szanonn@gmail.com>
2023-09-29 11:13:24 -0300

    feat: deleting global

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ import vuetify from "vite-plugin-vuetify";
 export default defineNuxtConfig({
   devtools: { enabled: false },
   ssr: false,
-  css: ["vuetify/lib/styles/main.sass", "~/assets/global.scss"],
+  css: ["vuetify/lib/styles/main.sass"],
   build: {
     transpile: ["vuetify"],
   },
